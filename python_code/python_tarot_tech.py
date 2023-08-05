07141f62bba3c8a4a6e7f023bf509b51920ef04a
Douglas Kuhn <douglas@composable.finance>
2023-08-05 12:50:51 -0400

    Create python_tarot_tech.py
    Python Tarot, for tech

---
+++ python_code/python_tarot_tech.py
@@ -0,0 +1,96 @@
+
+
+
+
+minor_arcana = {
+    # Wands
+    "Ace of Wands": "Inspiration, new opportunities, growth",
+    "Two of Wands": "Future planning, progress, decisions",
+    "Three of Wands": "Preparation, foresight, enterprise",
+    "Four of Wands": "Community, home, celebration",
+    "Five of Wands": "Conflict, disagreements, competition",
+    "Six of Wands": "Success, public recognition, progress",
+    "Seven of Wands": "Defence, perseverance, maintaining control",
+    "Eight of Wands": "Speed, action, air travel",
+    "Nine of Wands": "Courage, persistence, test of faith, resilience",
+    "Ten of Wands": "Burden, responsibility, hard work, stress",
+    "Page of Wands": "Enthusiasm, exploration, discovery, free spirit",
+    "Knight of Wands": "Energy, passion, lust, action",
+    "Queen of Wands": "Confidence, courage, determination",
+    "King of Wands": "Natural leader, vision, entrepreneur",
+
+    # Cups
+    "Ace of Cups": "New love, compassion, creativity",
+    "Two of Cups": "Unified love, partnership, mutual attraction",
+    "Three of Cups": "Celebration, friendship, creativity",
+    "Four of Cups": "Meditation, contemplation, apathy",
+    "Five of Cups": "Loss, regret, disappointment",
+    "Six of Cups": "Revisiting the past, childhood memories, innocence",
+    "Seven of Cups": "Choices, fantasy, illusion",
+    "Eight of Cups": "Escapism, disappointment, abandonment",
+    "Nine of Cups": "Contentment, satisfaction, gratitude",
+    "Ten of Cups": "Domestic bliss, harmony, joy",
+    "Page of Cups": "Creative opportunities, intuition, curiosity",
+    "Knight of Cups": "Romance, charm, imagination",
+    "Queen of Cups": "Emotional balance, compassion, intuition",
+    "King of Cups": "Emotional balance, diplomacy, calm",
+
+    # Swords
+    "Ace of Swords": "Breakthrough, clarity, sharp mind",
+    "Two of Swords": "Difficult decisions, weighing up options, an uneasy truce",
+    "Three of Swords": "Heartbreak, emotional pain, sorrow",
+    "Four of Swords": "Rest, recovery, contemplation",
+    "Five of Swords": "Unbridled ambition, win at all costs, sneakiness",
+    "Six of Swords": "Transition, change, rite of passage",
+    "Seven of Swords": "Betrayal, deception, getting away with something",
+    "Eight of Swords": "Isolation, self-imposed restriction, imprisonment",
+    "Nine of Swords": "Anxiety, worry, fear",
+    "Ten of Swords": "Endings, failure, collapse",
+    "Page of Swords": "Curiosity, restlessness, mental energy",
+    "Knight of Swords": "Action, impulsiveness, defending beliefs",
+    "Queen of Swords": "Complexity, clear mindedness, direct communication",
+    "King of Swords": "Clear thinking, intellectual power, authority",
+
+    # Pentacles
+    "Ace of Pentacles": "New financial or career opportunity, manifestation, abundance",
+    "Two of Pentacles": "Multiple priorities, time management, prioritisation",
+    "Three of Pentacles": "Teamwork, initial fulfilment, collaboration",
+    "Four of Pentacles": "Control, stability, security",
+    "Five of Pentacles": "Financial loss, poverty, lack mindset",
+    "Six of Pentacles": "Giving, receiving, sharing wealth, generosity",
+    "Seven of Pentacles": "Long-term view, sustainable results, perseverance",
+    "Eight of Pentacles": "Apprenticeship, repetitive tasks, mastery",
+    "Nine of Pentacles": "Abundance, luxury, self-sufficiency",
+    "Ten of Pentacles": "Wealth, financial security, family",
+    "Page of Pentacles": "Manifestation, financial opportunity, new job",
+    "Knight of Pentacles": "Efficiency, routine, conservatism",
+    "Queen of Pentacles": "Practicality, creature comforts, financial security",
+    "King of Pentacles": "Wealth, business, leadership, security"
+}
+
+
+
+major_arcana = {
+    "The Fool": "Launching a new software without testing",
+    "The Magician": "Master programmer with high versatility in coding languages",
+    "The High Priestess": "Deep understanding of machine learning and AI",
+    "The Empress": "Creation of a user-friendly and intuitive software",
+    "The Emperor": "Strong and stable systems architecture",
+    "The Hierophant": "Following the established coding standards and best practices",
+    "The Lovers": "The union of software and hardware in perfect harmony",
+    "The Chariot": "Successfully steering a project amidst various challenges",
+    "Strength": "Overcoming system vulnerabilities through strong cybersecurity measures",
+    "The Hermit": "In-depth data analysis and algorithm optimization",
+    "Wheel of Fortune": "Unexpected system updates, dealing with the ups and downs of software life cycle",
+    "Justice": "Net neutrality, data privacy, and digital rights",
+    "The Hanged Man": "Pausing to refactor code for better efficiency",
+    "Death": "End of a legacy system, transition to new technology",
+    "Temperance": "Balancing functionality and user experience",
+    "The Devil": "Dealing with malicious hacking, cyber threats",
+    "The Tower": "A system crash or major security breach",
+    "The Star": "Innovation, a new hope in technology",
+    "The Moon": "Navigating the mysteries of the Darknet",
+    "The Sun": "Successful launch of a product, widespread user adoption",
+    "Judgement": "System audit, code review, debugging",
+    "The World": "Completion of a software project, the achievement of a digital goal"
+}
